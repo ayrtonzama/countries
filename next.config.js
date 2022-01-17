10578c4d6cb41541c5f9ea249ab3bfb013b6683e
ayrton <ayrtonzama@gmail.com>
2022-01-17 17:10:37 +0200

    added sass to the project

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
-module.exports = {
+const path = require('path')
+const withSass = require('@zeit/next-sass');
+module.exports = withSass({
   reactStrictMode: true,
-}
+  cssModules: true
+})
+
+module.exports = {
+ 
+  sassOptions: {
+  includePaths: [path.join(__dirname, 'styles')],
+  },
+  }
+  
