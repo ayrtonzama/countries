9297814aa0bec8de9b36b64ff43b3bdecc84d90f
ayrton <ayrtonzama@gmail.com>
2022-01-19 16:06:40 +0200

    mobile screen responsive

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,30 +1,44 @@
-
-import { AppBar, Typography, Button, Toolbar,Container } from "@mui/material";
-import { useTheme } from '@mui/material/styles';
-import NightlightRoundIcon from '@mui/icons-material/NightlightRound';
-import LightModeIcon from '@mui/icons-material/LightMode';
-import {  useThemeUpdate } from '../hooks/ThemeContext';
+import { AppBar, Typography, Button, Toolbar, Container } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import NightlightRoundIcon from "@mui/icons-material/NightlightRound";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import { useThemeUpdate } from "../hooks/ThemeContext";
+import Link from "next/link";
 export default function NavBar() {
-  const theme=useTheme()
-  const themeToggle=useThemeUpdate()
+  const theme = useTheme();
+  const themeToggle = useThemeUpdate();
 
-  
   return (
-  
     <AppBar className="">
-        <Container disableGutters={false} maxWidth="xl">
-      <Toolbar>
-
-        <Typography variant="h5" sx={{ flexGrow: 1 }}>
-          <strong>Where in the world?</strong>
-        </Typography>
-        <Button 
-        startIcon={theme.palette.mode == "light" ?<NightlightRoundIcon/>:<LightModeIcon/>}
-        variant="text" onClick={themeToggle}  className={theme.palette.mode+'-text'} id="theme-btn">
-          {theme.palette.mode == "light" ? "Dark" : "Light"} Mode
-        </Button>
-      </Toolbar>
-        </Container>
+      <Container disableGutters={false} maxWidth="xl">
+        <Toolbar>
+          <Link href="/">
+            <Typography
+              variant="h5"
+              sx={{ flexGrow: 1 ,fontSize:{xs:16}}}
+              className="link-styles"
+            >
+              <strong>Where in the world?</strong>
+            </Typography>
+          </Link>
+          <Button
+            startIcon={
+              theme.palette.mode == "light" ? (
+                <NightlightRoundIcon />
+              ) : (
+                <LightModeIcon />
+              )
+            }
+            variant="text"
+            onClick={themeToggle}
+            className={theme.palette.mode + "-text"}
+            id="theme-btn"
+            sx={{fontSize:{xs:14}}}
+          >
+            {theme.palette.mode == "light" ? "Dark" : "Light"} Mode
+          </Button>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 }
--- pages/404.jsx
+++ pages/404.jsx
@@ -1,3 +1,11 @@
+import { Container,Typography } from "@mui/material";
+
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
+    return (<Container sx={{margin:'auto',marginTop:'4rem'}}>
+      <div className="center-content">
+
+      <Typography variant="h1" align="center">404</Typography>
+      <Typography variant="h2" align="center">Page Not Found</Typography>
+      </div>
+    </Container>)
   }
